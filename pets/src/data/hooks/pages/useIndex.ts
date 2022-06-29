784de45e3982aa9d3f409deea53d7d9e739298a9
Guilherme Bernardo <bguilherme51@gmail.com>
2022-06-29 13:32:39 -0400

    correção path do backend

--- pets/src/data/hooks/pages/useIndex.ts
+++ pets/src/data/hooks/pages/useIndex.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Pet } from '../../@types/Pet'
 import { ApiService } from '../../../services/ApiService'
+import { AxiosError } from 'axios';
 
 export function useIndex(){
 	const [listaPets, setListaPets] = useState<Pet[]>([]),
