af79eeb2381674885aaef53aae14269651397c99
Guilherme Bernardo <bguilherme51@gmail.com>
2022-07-19 19:41:19 -0400

    app finished

---
+++ pets/src/data/hooks/pages/pets/useRelatorio.ts
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import { Relatorio } from "../../../@types/Relatorio";
+import { ApiService } from '../../../../services/ApiService';
+
+export function UseRelatorio(){
+	const [listaRelatorio, setListaRelatorio] = useState<Relatorio[]>([]);
+
+	useEffect(() => {
+		ApiService.get('/adocoes').then((resposta) =>{
+			setListaRelatorio(resposta.data);
+		})
+	}, []);
+
+	return {listaRelatorio}
+}
--- pets/src/pages/pets/relatorio.tsx
+++ pets/src/pages/pets/relatorio.tsx
@@ -1,8 +1,50 @@
 import { NextPage } from 'next'
+import Title from '../../ui/components/Title/Title';
+import {
+	Paper,
+	TableContainer,
+	Table,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableBody
+} from '@mui/material'
+import { UseRelatorio }	 from '../../../src/data/hooks/pages/pets/useRelatorio'
 
 const Relatorio: NextPage = () => {
+	const { listaRelatorio } = UseRelatorio();
+
 	return (
-		<div>aaaa</div>
+		<>
+			<Title 
+			title={'Relatorio de adoção'}
+			subtitle={'Veja a lista de pets adotados'}
+			/>
+			<TableContainer
+				component={Paper}
+				sx={{maxWidth:830, mx: 'auto', p: {xs: 3, md: 5}}}
+			>
+				<Table>
+					<TableHead>
+						<TableRow>
+						<TableCell>Pet</TableCell>
+						<TableCell>E-mail</TableCell>
+						<TableCell align={'right'}>Valor Mensal</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{listaRelatorio.map((relatorio) => (
+							<TableRow key={relatorio.id}>
+								<TableCell>{relatorio.pet.nome}</TableCell>
+								<TableCell>{relatorio.email}</TableCell>
+								<TableCell align={'right'}>{relatorio.valor}</TableCell>
+							</TableRow>
+							))
+						}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</>
 	)
 }
 
